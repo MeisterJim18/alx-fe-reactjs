9dfedac5fc64d621cfb4f1ed97d5c6cc190a35dc
Jim Mariel Ngasseu Ngonna <jimmariel47@gmail.com>
2025-09-07 19:56:02 +0200

    save first task

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+     <main style={{ backgroundColor: '#f0f0f0', textAlign: 'center', padding: '20px' }}>
+      <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
