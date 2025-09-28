005785dc59dab6f8451b8e6bbb7a9ccddfab74f8
Jim Mariel Ngasseu Ngonna <jimmariel47@gmail.com>
2025-09-28 20:00:37 +0200

    task 1.6

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
-
 const githubService = {
   async fetchUserData(username) {
     try {
-      const response = await axios.get(`${GITHUB_API_URL}/users/${username}`);
+      const response = await axios.get(`https://api.github.com/users/${username}`);
       return response.data;
     } catch (error) {
       throw error;
