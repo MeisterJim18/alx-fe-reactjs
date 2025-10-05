e7c33b30d01febe54fa2a990497d66e6a6a4e595
Jim Mariel Ngasseu Ngonna <jimmariel47@gmail.com>
2025-10-05 21:33:24 +0200

    task 3: added interactive hover effects, transitions and animations

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-8 md:my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-8 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=150&h=150&fit=crop&crop=face" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto border-2 border-white shadow-md"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto border-2 border-white shadow-md hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center font-semibold">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center font-semibold hover:text-blue-500 transition-colors duration-300">
         John Doe
       </h1>
       <p className="text-gray-600 text-sm md:text-base text-center">
